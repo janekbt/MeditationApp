2d75d1e1ebb094a96a9bd34284b36e27dffc7fb3
Janek Teders <janekbt@users.noreply.github.com>
2026-04-10 17:32:27 +0200

    Fix IsA usage in window::new

--- src/window/mod.rs
+++ src/window/mod.rs
@@ -12,7 +12,7 @@ glib::wrapper! {
 }
 
 impl MeditateWindow {
-    pub fn new(app: &impl glib::IsA<adw::Application>) -> Self {
+    pub fn new(app: &impl IsA<adw::Application>) -> Self {
         glib::Object::builder()
             .property("application", app)
             .build()
